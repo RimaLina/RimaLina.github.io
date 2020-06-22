96301bfd6ff056b695954b8e70dc79f23522f36b
rimalinab <rima.linaburgyte@gmail.com>
2020-06-22 21:45:06 +0300

    Updates

---
+++ precache-manifest.b2bfc95baf9650d8c9f0581dff25b71e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76887ee999a70b3341c3c66b3bd2bdc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93b35b3454c967f7a396",
+    "url": "/static/css/main.e7791e7d.chunk.css"
+  },
+  {
+    "revision": "b0c4540da2c52d6e4cd4",
+    "url": "/static/js/2.6674d28d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6674d28d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93b35b3454c967f7a396",
+    "url": "/static/js/main.0beeffad.chunk.js"
+  },
+  {
+    "revision": "80e274ec0ec388e5e202",
+    "url": "/static/js/runtime-main.c8b434d9.js"
+  },
+  {
+    "revision": "dc54c6dacb9a82fccf5338d7b4e7dec5",
+    "url": "/static/media/CVRimaLinaburgyte.dc54c6da.pdf"
+  },
+  {
+    "revision": "4639d44ace9c25cedf860e12b4883b48",
+    "url": "/static/media/ai.4639d44a.png"
+  },
+  {
+    "revision": "8b8da0226d589d8fc0c9dbf331c736f0",
+    "url": "/static/media/book.8b8da022.png"
+  },
+  {
+    "revision": "5ed9ed9e8db2b162ea951bd263ca88d6",
+    "url": "/static/media/diet.5ed9ed9e.png"
+  },
+  {
+    "revision": "07414a1ec53d2d69a16b114e6878bd04",
+    "url": "/static/media/history.07414a1e.png"
+  },
+  {
+    "revision": "f6ec72b07a35a7a94734b21857b705d6",
+    "url": "/static/media/istqb.f6ec72b0.png"
+  }
+]);
