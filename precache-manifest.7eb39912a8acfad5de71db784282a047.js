d58dce8a17bf2d3fd71a16d341f4025623eb8bfe
rimalinab <rima.linaburgyte@gmail.com>
2020-06-22 21:49:01 +0300

    Updates

---
+++ precache-manifest.7eb39912a8acfad5de71db784282a047.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d440728862d0c4f2140b8e4fbe893d5",
+    "url": "/RimaLina.github.io/index.html"
+  },
+  {
+    "revision": "1e792f020e043bd2b66d",
+    "url": "/RimaLina.github.io/static/css/main.e7791e7d.chunk.css"
+  },
+  {
+    "revision": "6a6e375de2bb4f1e0c5e",
+    "url": "/RimaLina.github.io/static/js/2.b6ef6d0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RimaLina.github.io/static/js/2.b6ef6d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e792f020e043bd2b66d",
+    "url": "/RimaLina.github.io/static/js/main.0e9eb5be.chunk.js"
+  },
+  {
+    "revision": "662a4c857b50c03fe32b",
+    "url": "/RimaLina.github.io/static/js/runtime-main.922feb0a.js"
+  },
+  {
+    "revision": "dc54c6dacb9a82fccf5338d7b4e7dec5",
+    "url": "/RimaLina.github.io/static/media/CVRimaLinaburgyte.dc54c6da.pdf"
+  },
+  {
+    "revision": "4639d44ace9c25cedf860e12b4883b48",
+    "url": "/RimaLina.github.io/static/media/ai.4639d44a.png"
+  },
+  {
+    "revision": "8b8da0226d589d8fc0c9dbf331c736f0",
+    "url": "/RimaLina.github.io/static/media/book.8b8da022.png"
+  },
+  {
+    "revision": "5ed9ed9e8db2b162ea951bd263ca88d6",
+    "url": "/RimaLina.github.io/static/media/diet.5ed9ed9e.png"
+  },
+  {
+    "revision": "07414a1ec53d2d69a16b114e6878bd04",
+    "url": "/RimaLina.github.io/static/media/history.07414a1e.png"
+  },
+  {
+    "revision": "f6ec72b07a35a7a94734b21857b705d6",
+    "url": "/RimaLina.github.io/static/media/istqb.f6ec72b0.png"
+  }
+]);
