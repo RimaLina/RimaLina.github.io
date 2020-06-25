a3f43197a6c2f019dbfd59723219be8f9e82a6db
rimalinab <rima.linaburgyte@gmail.com>
2020-06-25 13:56:10 +0300

    Test

---
+++ precache-manifest.c0c568c229d398ea0dea07e5dec0df45.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a817514413eeb72fe564622bb25329e",
+    "url": "/RimaLina.github.io/index.html"
+  },
+  {
+    "revision": "cd8367f4b91937e03888",
+    "url": "/RimaLina.github.io/static/css/main.e7791e7d.chunk.css"
+  },
+  {
+    "revision": "878c2105cb948ed5c92e",
+    "url": "/RimaLina.github.io/static/js/2.a925bd4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RimaLina.github.io/static/js/2.a925bd4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd8367f4b91937e03888",
+    "url": "/RimaLina.github.io/static/js/main.ce7d3791.chunk.js"
+  },
+  {
+    "revision": "662a4c857b50c03fe32b",
+    "url": "/RimaLina.github.io/static/js/runtime-main.922feb0a.js"
+  },
+  {
+    "revision": "dc54c6dacb9a82fccf5338d7b4e7dec5",
+    "url": "/RimaLina.github.io/static/media/CVRimaLinaburgyte.dc54c6da.pdf"
+  },
+  {
+    "revision": "4639d44ace9c25cedf860e12b4883b48",
+    "url": "/RimaLina.github.io/static/media/ai.4639d44a.png"
+  },
+  {
+    "revision": "8b8da0226d589d8fc0c9dbf331c736f0",
+    "url": "/RimaLina.github.io/static/media/book.8b8da022.png"
+  },
+  {
+    "revision": "5ed9ed9e8db2b162ea951bd263ca88d6",
+    "url": "/RimaLina.github.io/static/media/diet.5ed9ed9e.png"
+  },
+  {
+    "revision": "07414a1ec53d2d69a16b114e6878bd04",
+    "url": "/RimaLina.github.io/static/media/history.07414a1e.png"
+  },
+  {
+    "revision": "f6ec72b07a35a7a94734b21857b705d6",
+    "url": "/RimaLina.github.io/static/media/istqb.f6ec72b0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b2bfc95baf9650d8c9f0581dff25b71e.js"
+  "/RimaLina.github.io/precache-manifest.c0c568c229d398ea0dea07e5dec0df45.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RimaLina.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
